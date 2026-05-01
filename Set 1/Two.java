27421033abe74f78205f2526dc263e1292eae6b1
Mohaiminul Hoque <saintman789@gmail.com>
2026-05-01 23:40:55 +0600

    Set 1 done

---
+++ Set 1/Two.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Two{
+	public static void main(String[] args){
+		Scanner helper = new Scanner(System.in);
+		System.out.print("Your Name:Mohaiminul Hoque;\n Hello,Mohaimin ");
+		String name = helper.nextLine();
+
+		System.out.println("Hello, " + name);
+	}
+}
